ea77b3bdd424bc9a3da378a733941f398cfa93c8
Karina Muñoz Gonzalez <karinamzalez@gmail.com>
2017-04-19 17:50:14 -0600

    add mapDispatch to props to IdeaItem component * add remove button

--- src/components/IdeaItem.js
+++ src/components/IdeaItem.js
@@ -1,6 +1,8 @@
 /*jshint esversion: 6 */
 
 import React from 'react';
+import { removeIdea } from '../actions/index.js';
+import { connect } from 'react-redux';
 // import stylesheet
 
 const IdeaItem = (props) => {
@@ -9,8 +11,21 @@ const IdeaItem = (props) => {
       <h1>{props.idea.title}</h1>
       <p>{props.idea.body}</p>
       <p>{props.idea.quality}</p>
+      <button className="delete-btn" onClick={(e) => props.removeIdea(props.idea.id, e)}>Remove Idea</button>
     </li>
   );
 };
 
-export default IdeaItem;
+const mapDispatchToProps = dispatch => {
+  return {
+    addIdea: (id, e) => {
+      e.preventDefault()
+      const id = id
+    }
+    dispatch(removeIdea(id))
+  }
+}
+
+export default connect(
+  mapDispatchToProps
+)(IdeaItem);
